59ddec9cdb4c969fed0a9add15fd7defa745ff47
Martin Bergek <martin@spotwise.com>
2016-08-22 15:28:22 +0200

    Fix custom keys

--- sixteen-eight.rb
+++ sixteen-eight.rb
@@ -85,7 +85,6 @@ rescue LoadError
 No custom keys found. Copy the file development_keys.orig.rb to development_keys.rb and define your own keys.
 
 EOS
-  exit
 end
 
 append_file "Gemfile" do <<-'FILE'
